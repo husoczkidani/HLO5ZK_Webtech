e747509a4daef26bc6f263c339849efa2887fba0
husoczkidani <71039398+husoczkidani@users.noreply.github.com>
2021-10-15 11:14:43 +0200

    reupload

--- Webtech1/scripts/light_dark_mode.js
+++
@@ -1,9 +0,0 @@
-function modeSwitch() {
-    var element1 = document.body;
-    if(element1.classList.contains("bg-bright-theme"))
-    {
-        element1.classList.toggle("bg-dark-theme")
-    }else{
-        element1.classList.toggle("bg-bright-theme")
-    }
- }
