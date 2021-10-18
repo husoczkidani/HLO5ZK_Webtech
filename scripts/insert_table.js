ef6bbdde7d0260a26d2bf3bace1cd7c1b6ccabd2
Husóczki Dániel <husoczkidaniel@gmail.com>
2021-10-18 22:03:28 +0200

    updated carousel with brand logos, enchanced table form

--- scripts/insert_table.js
+++ scripts/insert_table.js
@@ -1,10 +1,32 @@
 function insertNewLine() {
     var table = document.getElementById("section2Table");
     var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = $("#text").val();
-    cell2.innerHTML = $("#text").val();
-    cell3.innerHTML = $("#text").val();
+    var brand = row.insertCell(0);
+    var type = row.insertCell(1);
+    var fuel = row.insertCell(2);
+    var year = row.insertCell(3);
+    var performance = row.insertCell(4);
+    brand.innerHTML = $("#brand").val();
+    type.innerHTML = $("#type").val();
+    fuel.innerHTML = $("#fuel").val();
+    year.innerHTML = $("#year").val();
+    performance.innerHTML = ($("#performanceTextKW").val() + "Kw, "+ $("#performanceTextHP").val()+ "Hp");
+    
 }
+
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
