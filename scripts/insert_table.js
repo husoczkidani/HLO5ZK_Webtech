30e95a70a566b321d9f40edf539172f76f2098ed
husoczkidani <71039398+husoczkidani@users.noreply.github.com>
2021-11-01 14:23:55 +0100

    updated insert tablem with validation

--- scripts/insert_table.js
+++ scripts/insert_table.js
@@ -1,32 +1,43 @@
-function insertNewLine() {
+$("#formBtn").click(function(){
+  var brand = $("#brand").val();
+  var type = $("#type").val();
+  var fuel = $("#fuel").val();
+  var year = $("#year").val();
+  var performanceKW = $("#performanceTextKW").val();
+  var performanceHP = $("#performanceTextHP").val();
+  var performanceSlider = $("#performanceSlider").val();
+  var isNull = brand === "" 
+            || type === "" 
+            || fuel === "" 
+            || year === "" 
+            || performanceKW === "" 
+            || performanceHP === ""
+            || performanceSlider === "";
+  if(!isNull){
     var table = document.getElementById("section2Table");
     var row = table.insertRow(0);
-    var brand = row.insertCell(0);
-    var type = row.insertCell(1);
-    var fuel = row.insertCell(2);
-    var year = row.insertCell(3);
-    var performance = row.insertCell(4);
-    brand.innerHTML = $("#brand").val();
-    type.innerHTML = $("#type").val();
-    fuel.innerHTML = $("#fuel").val();
-    year.innerHTML = $("#year").val();
-    performance.innerHTML = ($("#performanceTextKW").val() + "Kw, "+ $("#performanceTextHP").val()+ "Hp");
-    
+    var brandRow = row.insertCell(0);
+    var typeRow = row.insertCell(1);
+    var fuelRow = row.insertCell(2);
+    var yearRow = row.insertCell(3);
+    var performanceRow = row.insertCell(4);
+    brandRow.innerHTML = brand
+    typeRow.innerHTML = type
+    fuelRow.innerHTML = fuel
+    yearRow.innerHTML = year
+    performanceRow.innerHTML = (performanceKW + "Kw, "+ performanceHP+ "Hp");
+    clearForm();
+  }else{
+    $("#brandLabel").css("color:red;")
+    alert("Fill everything!");
+  }
+})
+function clearForm(){
+    $("#brand").val("");
+    $("#type").val("");
+    $("#fuel").val("");
+    $("#year").val("");
+    $("#performanceTextKW").val(0);
+    $("#performanceTextHP").val(0);
+    $("#performanceSlider").val(0);
 }
-
-(function () {
-    'use strict'
-    var forms = document.querySelectorAll('.needs-validation')
-
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-            
-          }
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
