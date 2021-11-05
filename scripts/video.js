1399184efb1ef223eff1c9ef194408cecc4c0971
Husóczki Dániel <husoczkidaniel@gmail.com>
2021-11-05 20:59:56 +0100

    car events
    added 2 video with animations in section3

--- scripts/video.js
+++ scripts/video.js
@@ -1,5 +1,4 @@
 var video = document.getElementById("video");
-var btn = document.getElementById("videoBtn");
 var control = document.getElementById("videoController");
 
 $('#videoController').on('click',function videoController() {
@@ -12,4 +11,31 @@ $('#videoController').on('click',function videoController() {
     control.classList.remove("fa-pause")
     control.classList.add("fa-play")
   }
+});
+
+var rally_video = document.getElementById("rally_video");
+var drift_video = document.getElementById("drift_video");
+
+
+$(document).ready(function(){
+    $('#rally_div').hover(function() {
+        $(this).addClass('transition');
+        $("#rally_img").fadeOut();
+        rally_video.play();
+    }, function() {
+        $(this).removeClass('transition');
+        $("#rally_img").fadeIn();
+        rally_video.pause();
+
+    });
+
+    $('#drift_div').hover(function() {
+      $(this).addClass('transition');
+      $("#drift_img").fadeOut();
+      drift_video.play();
+  }, function() {
+      $(this).removeClass('transition');
+      $("#drift_img").fadeIn();
+      drift_video.pause();
+  });
 });
